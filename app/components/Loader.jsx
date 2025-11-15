7e6be50f4ab1854de4e134104ce2b6acca6a3b92
sanjay-sk-99 <sanjay.g8699@gmail.com>
2025-11-15 17:46:06 +0530

    fix/swipeble animation fixed on income expense list

--- app/components/Loader.jsx
+++ app/components/Loader.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Loader = () => {
   return (
      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="large" color="#9333ea" />
       </View>
   )
 }
